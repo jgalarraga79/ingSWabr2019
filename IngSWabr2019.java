44239463e085c3b880438402b69832424b0b470b
jgalarraga79 <45775122+jgalarraga79@users.noreply.github.com>
2019-07-09 14:47:04 -0500

    Add files via upload

--- IngSWabr2019.java
+++ IngSWabr2019.java
@@ -21,24 +21,25 @@ public class IngSWabr2019 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    
-    Scanner sc = new Scanner(System.in);
-                
-        int fibo1,fibo2,i,entero;
         
+        //declaraion de variables
+        Scanner sc = new Scanner(System.in);      
+        int fibo1,fibo2,i,entero;
+        //ingreso de datos numericos
         do{
             System.out.print("Ingrese entero mayor que 1 y menor o igual que 100: ");
             entero = sc.nextInt();
         }while(entero<=1 || entero>100);   
-        
+        //declaracion de archivo para grabar
         FileWriter fw;
         
         try {
+            //grabado de datos
             fw = new FileWriter(new File("serie.txt"),true);
             fw.write("Los " + entero + " primeros términos de la serie de Fibonacci son:");
+            //calculo de serie Fibonacci
             fibo1=1;
             fibo2=1; 
-
             fw.write(fibo1 + " ");
             for(i=2;i<=entero;i++){
                  fw.write(fibo2 + " ");
@@ -46,7 +47,7 @@ public class IngSWabr2019 {
                  fibo1 = fibo2 - fibo1;
             }
             fw.write(System.lineSeparator());
-
+            //cerrar archivo
             fw.close();
         } catch (IOException ex) {
             }
